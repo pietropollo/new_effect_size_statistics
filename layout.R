c2e807ed19680e721a019eae970ec11cc021358a
pietropollo <pietro_pollo@hotmail.com>
2025-03-01 23:40:16 +1100

    add data and analysis

---
+++ layout.R
@@ -0,0 +1,43 @@
+# layout <-
+#   "
+#   ABCDE
+#   ABCDE
+#   ABCDE
+#   ABCDE
+#   ABCDE
+#   ABCDE
+#   FGHIJ
+#   FGHIJ
+#   FGHIJ
+#   FGHIJ
+#   FGHIJ
+#   FGHIJ
+#   KKK##
+#   LLL##
+#   MMM##
+#   NNN##
+#   OOO##
+#   PPP##
+# "
+
+layout <-
+  "
+  AH
+  BH
+  CH
+  DH
+  EH
+  FH
+  #H
+"
+
+layout_2 <-
+  "
+  AH#IP
+  BH#JP
+  CH#KP
+  DH#LP
+  EH#MP
+  FH#NP
+  #H##P
+"
