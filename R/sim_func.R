c9dc7e168fc31752a2db2744631f193ec9b3dcf5
Daniel Noble <daniel.noble@anu.edu.au>
2025-07-02 20:44:21 +1000

    First pass simulation for skewness and kutosis

--- R/sim_func.R
+++ R/sim_func.R
@@ -7,22 +7,23 @@
 	pacman::p_load(moments, PearsonDS)
 
 # Parameters
-	          n <- c(10, 25, 50, 75, 100, 150) 
-	    scenarios  <- data.frame(mean_g1 = c(0, 5, 10, 15, 20),
-	    mean_g2 = c(0, 5, 10, 15, 20),
-	variance_g1 = c(1, 2, 3, 4, 5),
-	variance_g2 = c(1, 2, 3, 4, 5),
-	skewness_g1 = c(1, 1.5, 2, 3, 4),
-	skewness_g2 = c(1, 1.5, 2, 3, 4),
-	kurtosis_g1 = c(3, 4, 5, 6, 7),
-	kurtosis_g2 = c(3, 4, 5, 6, 7))
-          nsims <- 5000  # Number of simulations
+	                                      n = c(10, 25, 50, 75, 100, 150) 
+	    							mean_g1 = c(0, 0, 0)
+									mean_g2 = c(0, 10, 20)
+								variance_g1 = c(2)
+								variance_g2 = c(2)
+								skewness_g1 = c(1, 1.5, 2, 3, 4)
+								skewness_g2 = c(1, 1.5, 2, 3, 4)
+								kurtosis_g1 = c(3, 6)
+								kurtosis_g2 = c(3, 6)
+		scenarios <- expand.grid(mean_g1 = mean_g1, mean_g2 = mean_g2, variance_g1 = variance_g1, variance_g2 = variance_g2, skewness_g1 = skewness_g1, skewness_g2 = skewness_g2, kurtosis_g1 = kurtosis_g1, kurtosis_g2 = kurtosis_g2) # Create all combinations of scenarios
+          nsims <- 1000  # Number of simulations
 
 # Create combinations of parameters expanded by sample size vector. Each row is a scenario with a sample size which is used to set up the simulation
 	params_all <- data.frame(tidyr::crossing(scenarios, n = n))
 
 # functions for calculating effect sizes ----
-## skewness ----
+## skewness ---- !!!!# Need to check because this function does not match `moments::skewness`
 calc.skewness <- function(x, output = "est") {
   n <- length(x)
   
@@ -37,7 +38,7 @@ calc.skewness <- function(x, output = "est") {
   }
 }
 
-## kurtosis ----
+## kurtosis ---- !!!!# Need to check because this function does not match `moments::kurtosis`
 calc.kurtosis <- function(x, output = "est") {
   n <- length(x)
   
@@ -52,27 +53,25 @@ calc.kurtosis <- function(x, output = "est") {
 }
 
 
-#| @title Simulate Pearson distribution with equal sample sizes 
-#| @description Function to simulate Pearson distribution with equal sample sizes
+#| @title Simulate Pearson distribution with specified moments
+#| @description Function to simulate Pearson distribution with defined moments: mean, variance, skewness and kurtosis
 #| and calculate bias and coverage
-#| @param n Sample size for each group
-#| @param params Data frame containing parameters for the simulation. Note that this is from a params data frame created by the expand.grid function above and so included data from a single row which is a single scenario.
+#| @param params Data frame containing parameters for the simulation. Note that this is from a params data frame created by the expand.grid function above and so included data from a single row which is a single scenario. Params must define the sample size and the moments for both groups (mean_g1, mean_g2, variance_g1, variance_g2, skewness_g1, skewness_g2, kurtosis_g1, kurtosis_g2).
 #| @param nsims Number of simulations to run (default is 1000)
-#| @return A data frame with bias and coverage for each simulation
+#| @return A data frame with bias with respect to point estimates of effect size and its sampling variance for each scenario.
 #| @examples
 #| sim_func(n = 50, params = params[i,], nsims = 1000)
 
-sim_func <- function(n, params, nsims = nsims) {
+sim_func <- function(params, nsims = nsims) {
   # Vectors to store results
-      bias_sk <- numeric(nsims)
-	  bias_ku <- numeric(nsims)
-     coverage <- numeric(nsims)
-  
+      sk <- numeric(nsims)
+	  ku <- numeric(nsims)
+
 for(i in 1:nsims) {
 ##---------------------------##
   # Simulate data for group 1
 ##---------------------------##
-  x1 <- tryCatch(rpearson(n, 
+  x1 <- tryCatch(rpearson(params$n, 
 				 moments = c(    mean = params$mean_g1, 
 							 variance = params$variance_g1, 
 							 skewness = params$skewness_g1, 
@@ -85,7 +84,7 @@ for(i in 1:nsims) {
 ##---------------------------##
   # Simulate data for group 2
 ##---------------------------##
-  x2 <- tryCatch(rpearson(n, 
+  x2 <- tryCatch(rpearson(params$n, 
 				 moments = c(    mean = params$mean_g2, 
 							 variance = params$variance_g2, 
 							 skewness = params$skewness_g2, 
@@ -96,29 +95,22 @@ for(i in 1:nsims) {
                  })
 
 ##-------------------------------------------------##
-  # Calculate bias and coverage for effect statistics
+  # Calculate effect statistics
 ##-------------------------------------------------##
-## BELOW IS JUST AN EXAMPLE WE NEED TO MODIFY FOR VARIOUS EFFECTS
-# Calculate bias between groups, which is how much the difference between estimated sk and kur deviates from the true differences
-  bias_sk[i] = (calc.skewness(x1) - calc.skewness(x2)) - (params$skewness_g1 - params$skewness_g2)
-  bias_ku[i] = (calc.kurtosis(x1) - calc.kurtosis(x2)) - (params$kurtosis_g1 - params$kurtosis_g2)
 
-# Calculate coverage, which is the proportion of times the confidence interval contains the true difference
-           ci <- tryCatch(t.test(x1, x2)$conf.int, error = function(e) {
-							message("Error in t.test: ", e)
-							return(NA)
-							})
-  coverage[i] <- ifelse(ci[1] <= (params$mean_g1 - params$mean_g2) && 
-					    ci[2] >= (params$mean_g1 - params$mean_g2), 1, 0)
-
+#Calculate and store the effect statistics for skewness and kurtosis
+  sk[i] = tryCatch((calc.skewness(x1) - calc.skewness(x2)), error = function(e) {return(NA)}) #!!! CHECK THIS FUNCTION does not match `moments::skewness`
+  ku[i] = tryCatch((calc.kurtosis(x1) - calc.kurtosis(x2)), error = function(e) {return(NA)}) #!!! CHECK THIS FUNCTION does not match `moments::kurtosis`
 }
   
 ##-------------------------------------------------##
   # Return data with all the simulation results
 ##-------------------------------------------------##
-  return(data.frame(bias = mean(bias1, na.rm = TRUE), 
-  				coverage = sum(coverage, na.rm = TRUE) / nsims,
-					   n = length(bias1)))
+  return(data.frame(bias_sk = mean(sk) - (params$skewness_g1 - params$skewness_g2), # Bias for skewness from true value
+  				    bias_ku = mean(ku) - (params$kurtosis_g1 - params$kurtosis_g2), # Bias for kurtosis from true value
+				 bias_sk_sv = sd(sk)^2 - (calc.skewness(x1, output = "var") + calc.skewness(x2, output = "var")), # Bias for skewness sampling variance from analytical approximation
+				 bias_ku_sv = sd(ku)^2 - (calc.kurtosis(x1, output = "var") + calc.kurtosis(x2, output = "var")), # Bias for kurtosis sampling variance from analytical approximation
+					 n_sims = nsims)) 
 }
 
 ###------------------------------------------------------------------------###
@@ -130,6 +122,6 @@ result <- data.frame()
 # Loop through each scenario and run the simulation function
 for(i in 1:nrow(params_all)) {
   params <- params_all[i,]
-  result <- rbind(result, sim_func(n = params$n, params = params, nsims = nsims))
-  print(paste("Simulation for scenario", i, "completed. Bias:", round(result$bias, 2), "Coverage:", round(result$coverage, 2)))
+  result <- rbind(result, sim_func(params = params, nsims = nsims))
+  print(paste("Simulation for scenario", i, "completed. Bias_sk:", round(result$bias_sk[i], 2), "Bias_ku:", round(result$bias_ku[i], 2)))
 }
