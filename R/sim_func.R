c75abdc2481d1e97f1f9f644ccc4c5d96b608cf6
Daniel Noble <daniel.noble@anu.edu.au>
2025-07-03 10:16:14 +1000

    Fixing sampling variance bias and also need to fix kurtosis

--- R/sim_func.R
+++ R/sim_func.R
@@ -7,7 +7,7 @@
 	pacman::p_load(moments, PearsonDS)
 
 # Parameters
-	                                      n = c(10, 25, 50, 75, 100, 150) 
+	                        n = c(10, 25, 50, 75, 100, 150) 
 	    							mean_g1 = c(0, 0, 0)
 									mean_g2 = c(0, 10, 20)
 								variance_g1 = c(2)
@@ -43,13 +43,21 @@ calc.kurtosis <- function(x, output = "est") {
   n <- length(x)
   
   if (output == "est") { # kurtosis estimate
-    ((((n + 1) * n * (n - 1)) / ((n - 2) * (n - 3))) *
-       (sum((x - mean(x)) ^ 4) / (sum((x - mean(x)) ^ 2) ^ 2))) -
-      (3 * ((n - 1) ^ 2) / ((n - 2) * (n - 3)))
+    (((n + 1) * n) / ((n - 1) * (n - 2) * (n - 3))) *
+          (sum((x - mean(x))^4) / (sum((x - mean(x))^2)^2)) - (3 * (n - 1)^2 / ((n - 2) * (n - 3)))
   } else if (output == "var") { # kurtosis sampling variance
     (24 * n * ((n - 1) ^ 2)) /
       ((n - 3) * (n - 2) * (n + 3) * (n + 5))
   }
+}
+
+kurtosis_uncorrected <- function(x) {
+  n <- length(x)
+  x_bar <- mean(x)
+  s <- sd(x)  # sample standard deviation uses n - 1
+  m4 <- mean((x - x_bar)^4)
+  kurt <- m4 / s^4
+  return(kurt)
 }
 
 # Little function to summaries all the moments of a distribution. Used for testing
@@ -58,12 +66,15 @@ dist_summary <- function(x) {
 	mean = mean(x, na.rm = TRUE),
 	variance = var(x, na.rm = TRUE),
 	skewness = moments::skewness(x),
-	kurtosis = moments::kurtosis(x)
+  skewness_ours = calc.skewness(x, output = "est"),
+	kurtosis = moments::kurtosis(x),
+  kurtosis_ours = calc.kurtosis(x, output = "est")
   )
 }
+dist_summary(x1)
 
 # Testing params object # out when done. This would give normal distribution with slight skewness 
-params <- data.frame(mean_g1 = 0, mean_g2 = 0, variance_g1 = 1, variance_g2 = 1, skewness_g1 = 1, skewness_g2 = 1.5, kurtosis_g1 = 6, kurtosis_g2 = 6, n = 1000000)
+params <- data.frame(mean_g1 = 0, mean_g2 = 0, variance_g1 = 1, variance_g2 = 1, skewness_g1 = 1, skewness_g2 = 1.5, kurtosis_g1 = 6, kurtosis_g2 = 6, n = 100)
 
 #| @title Simulate Pearson distribution with specified moments
 #| @description Function to simulate Pearson distribution with defined moments: mean, variance, skewness and kurtosis
@@ -111,18 +122,20 @@ for(i in 1:nsims) {
 ##-------------------------------------------------##
 
 #Calculate and store the effect statistics for skewness and kurtosis
-  sk[i] = tryCatch((calc.skewness(x1) - calc.skewness(x2)), error = function(e) {return(NA)}) #!!! CHECK THIS FUNCTION does not match `moments::skewness`
-  ku[i] = tryCatch((calc.kurtosis(x1) - calc.kurtosis(x2)), error = function(e) {return(NA)}) #!!! CHECK THIS FUNCTION does not match `moments::kurtosis`
+  sk[i] = tryCatch((calc.skewness(x1) - calc.skewness(x2)), error = function(e) {return(NA)})
+  sk_sv[i] = tryCatch((calc.skewness(x1, output = "var") + calc.skewness(x2, output = "var")), error = function(e) {return(NA)}) #!!! CHECK THIS FUNCTION does not match `moments::skewness`
+  ku[i] = tryCatch((calc.kurtosis(x1) - calc.kurtosis(x2)), error = function(e) {return(NA)})
+  ku_sv[i] = tryCatch((calc.kurtosis(x1, output = "var") - calc.kurtosis(x2, output = "var")), error = function(e) {return(NA)}) #!!! CHECK THIS FUNCTION does not match `moments::kurtosis`
 }
   
 ##-------------------------------------------------##
   # Return data with all the simulation results
 ##-------------------------------------------------##
   return(data.frame(bias_sk = mean(sk) - (params$skewness_g1 - params$skewness_g2), # Bias for skewness from true value
-  				    bias_ku = mean(ku) - (params$kurtosis_g1 - params$kurtosis_g2), # Bias for kurtosis from true value
-				 bias_sk_sv = sd(sk)^2 - (calc.skewness(x1, output = "var") + calc.skewness(x2, output = "var")), # Bias for skewness sampling variance from analytical approximation
-				 bias_ku_sv = sd(ku)^2 - (calc.kurtosis(x1, output = "var") + calc.kurtosis(x2, output = "var")), # Bias for kurtosis sampling variance from analytical approximation
-					 n_sims = nsims)) 
+  				          bias_ku = mean(ku) - (params$kurtosis_g1 - params$kurtosis_g2), # Bias for kurtosis from true value
+				         bias_sk_sv = ((mean(sk_sv) - sd(sk)^2) / sd(sk)^2)*100, # Relative Bias for skewness sampling variance from analytical approximation
+				         bias_ku_sv = ((mean(ku_sv) - sd(ku)^2) / sd(ku)^2)*100, # Relative Bias for kurtosis sampling variance from analytical approximation
+					           n_sims = nsims)) 
 }
 
 ###------------------------------------------------------------------------###
