dbaba790a542572e1d5cea1c8699934dcbcf8ca4
Daniel Noble <daniel.noble@anu.edu.au>
2025-07-26 12:17:56 +1000

    Some fixes were needed with cor as bias was calculated within loop and probably bettera nd easier after once all simulations are done. Consistent with other sim function

--- R/sim_cor.R
+++ R/sim_cor.R
@@ -9,6 +9,7 @@ rm(list = ls()) # Remove all objects from the environment
 # Load required packages
 	pacman::p_load(moments, PearsonDS, MASS)
    source("./R/func.R") # Load the functions from func.R
+   source("./R/related_functions.R")
 
 
   ## Zr ----
@@ -26,14 +27,14 @@ zr.variance <- # Zr variance
 
 sim_cor <- function(params, nsims = nsims) {
   # Vectors to store results
-      bias_d_cor <- numeric(nsims)
-   bias_d_cor_sv <- numeric(nsims)
+      d_cor <- numeric(nsims)
+   d_cor_sv <- numeric(nsims)
 
-   boot_cor_bc <- numeric(nsims)
-   boot_cor_sv <- numeric(nsims)
-   jack_cor_bc <- numeric(nsims)
-   jack_cor_sv <- numeric(nsims)
-  
+   boot_d_cor_bc <- numeric(nsims)
+   boot_d_cor_sv <- numeric(nsims)
+   jack_d_cor_bc <- numeric(nsims)
+   jack_d_cor_sv <- numeric(nsims)
+
 for(i in 1:nsims) {
 ##---------------------------##
   # Simulate data for group 1
@@ -60,9 +61,9 @@ for(i in 1:nsims) {
 ##-------------------------------------------------##
   # Calculate bias 
 ##-------------------------------------------------##
-# Calculate bias between groups, which is how much the difference between correlations deviates from the true difference in values
-     bias_d_cor[i] = (r.to.zr(cor(g1)[1,2]) - r.to.zr(cor(g2)[1,2])) - (r.to.zr(params$cor_g1) - r.to.zr(params$cor_g2))
-  bias_d_cor_sv[i] = (1 / (params$n - 3)) + (1 / (params$n - 3)) # sampling variance of the difference in correlations
+# Calculate the difference between correlations and combined sampling variance
+     d_cor[i] = (r.to.zr(cor(g1)[1,2]) - r.to.zr(cor(g2)[1,2])) 
+  d_cor_sv[i] = (1 / (params$n - 3)) + (1 / (params$n - 3)) # sampling variance of the difference in correlations
 
   ## Add jacknife and bootstrap methods
   g1_cor_boot <- tryCatch(boot_cor(g1), error = function(e) {return(NA)})
@@ -70,29 +71,32 @@ for(i in 1:nsims) {
   g1_cor_jack <- tryCatch(jack_cor(g1), error = function(e) {return(NA)})
   g2_cor_jack <- tryCatch(jack_cor(g2), error = function(e) {return(NA)})
 
-  ## Calculate the bias-corrected estimates
-  boot_cor_bc[i] <- g1_cor_boot$est_bc - g2_cor_boot$est_bc
-  boot_cor_sv[i] <- g1_cor_boot$var + g2_cor_boot$var
-  jack_cor_bc[i] <- g1_cor_jack$est_bc - g2_cor_jack$est_bc
-  jack_cor_sv[i] <- g1_cor_jack$var + g2_cor_jack$var
+  ## Calculate the bias-corrected estimates and sampling variances from bootstraps and jackknife
+       boot_d_cor_bc[i] <- (g1_cor_boot$est_bc - g2_cor_boot$est_bc)
+       boot_d_cor_sv[i] <- (g1_cor_boot$var + g2_cor_boot$var)
+  
+       jack_d_cor_bc[i] <- (g1_cor_jack$est_bc - g2_cor_jack$est_bc) 
+       jack_d_cor_sv[i] <- (g1_cor_jack$var + g2_cor_jack$var)
 }
   
 ##-------------------------------------------------##
   # Return data with all the simulation results
 ##-------------------------------------------------##
-  return(data.frame(          bias_d_cor = mean(bias_d_cor, na.rm = TRUE),
-                           bias_d_cor_sv = ((mean(bias_d_cor_sv) - sd(bias_d_cor)^2) / sd(bias_d_cor)^2)*100,
-                            mcse_bias_sv = sqrt(var(bias_d_cor_sv, na.rm = TRUE) / nsims),
-                               mcse_bias = sqrt(var(bias_d_cor, na.rm = TRUE) / nsims),
-                           bias_boot_d_cor = mean(boot_cor_bc, na.rm = TRUE),
-                        bias_boot_d_cor_sv = ((mean(boot_cor_sv) - sd(bias_d_cor)^2) / sd(bias_d_cor)^2)*100,
-                        mcse_boot_d_cor_sv = sqrt(var(boot_cor_sv, na.rm = TRUE) / nsims),
-                           mcse_boot_d_cor = sqrt(var(boot_cor_bc, na.rm = TRUE) / nsims),
-                           bias_jack_d_cor = mean(jack_cor_bc, na.rm = TRUE),
-                        bias_jack_d_cor_sv = ((mean(jack_cor_sv) - sd(bias_d_cor)^2) / sd(bias_d_cor)^2)*100,
-                        mcse_jack_d_cor_sv = sqrt(var(jack_cor_sv, na.rm = TRUE) / nsims),
-                           mcse_jack_d_cor = sqrt(var(jack_cor_bc, na.rm = TRUE) / nsims),
-                                         n = length(bias_d_cor)))
+  return(data.frame(          bias_d_cor = (mean(d_cor, na.rm = TRUE) - (r.to.zr(params$cor_g1) - r.to.zr(params$cor_g2))),
+                           bias_d_cor_sv = ((mean(d_cor_sv) - sd(d_cor)^2) / sd(d_cor)^2)*100,
+                            mcse_bias_sv = sqrt(var(d_cor_sv, na.rm = TRUE) / nsims),
+                               mcse_bias = sqrt(var(d_cor, na.rm = TRUE) / nsims),
+
+                           bias_boot_d_cor = (mean(boot_d_cor_bc, na.rm = TRUE) - (r.to.zr(params$cor_g1) - r.to.zr(params$cor_g2))),
+                        bias_boot_d_cor_sv = ((mean(boot_d_cor_sv) - sd(d_cor)^2) / sd(d_cor)^2)*100,
+                        mcse_boot_d_cor_sv = sqrt(var(boot_d_cor_sv, na.rm = TRUE) / nsims),
+                           mcse_boot_d_cor = sqrt(var(boot_d_cor_bc, na.rm = TRUE) / nsims),
+
+                           bias_jack_d_cor = (mean(jack_d_cor_bc, na.rm = TRUE) - (r.to.zr(params$cor_g1) - r.to.zr(params$cor_g2))),
+                        bias_jack_d_cor_sv = ((mean(jack_d_cor_sv) - sd(d_cor)^2) / sd(d_cor)^2)*100,
+                        mcse_jack_d_cor_sv = sqrt(var(jack_d_cor_sv, na.rm = TRUE) / nsims),
+                           mcse_jack_d_cor = sqrt(var(jack_d_cor_bc, na.rm = TRUE) / nsims),
+                                         n = length(d_cor)))
 }
 
 ###------------------------------------------------------------------------###
@@ -106,7 +110,7 @@ for(i in 1:nsims) {
 	                n <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 500) 
 	    	     cor_g1 <- c(-0.8, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8)
              cor_g2 <- c(-0.8, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8)
-              nsims <- 5000  # Number of simulations
+              nsims <- 2500  # Number of simulations
 
 # Create combinations of parameters expanded by sample size vector. Each row is a scenario with a sample size which is used to set up the simulation
 	       scenarios <- expand.grid(cor_g1 = cor_g1, cor_g2 = cor_g2)
