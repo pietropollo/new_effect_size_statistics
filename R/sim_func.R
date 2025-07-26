9fa3789de51bcec6aa2c54b4c1ce31acfcd2a680
Daniel Noble <daniel.noble@anu.edu.au>
2025-07-26 10:58:40 +1000

    Fixes and also a few checks

--- R/sim_func.R
+++ R/sim_func.R
@@ -67,17 +67,15 @@ sim_func <- function(params, nsims = nsims, type = c("skewness", "kurtosis")) {
       sk_sv <- numeric(nsims)
       ku_sv <- numeric(nsims)
  
- skew_delta <- numeric(nsims) # Delta method for skewness
- kurt_delta <- numeric(nsims) # Delta method for kurtosis
-  boot_skew <- numeric(nsims) # Bootstrap method for skewness
+ # Point estimates for skewness and kurtosis
+    skew_delta <- numeric(nsims) # Delta method for skewness
+    kurt_delta <- numeric(nsims) # Delta method for kurtosis
   boot_skew_bc <- numeric(nsims) # Bootstrap bias-corrected method for skewness
-  boot_kurt <- numeric(nsims) # Bootstrap method for kurtosis
   boot_kurt_bc <- numeric(nsims) # Bootstrap bias-corrected method for kurtosis
-  jack_skew <- numeric(nsims) # Jackknife method for skewness
-  jack_kurt <- numeric(nsims) # Jackknife method for kurtosis
   jack_skew_bc <- numeric(nsims) # Jackknife bias-corrected method for skewness
   jack_kurt_bc <- numeric(nsims) # Jackknife bias-corrected method for kurtosis
 
+# Sampling variance for skewness and kurtosis
   skew_delta_sv <- numeric(nsims) # Delta method sampling variance for skewness
   kurt_delta_sv <- numeric(nsims) # Delta method sampling variance for kurtosis
   boot_skew_sv <- numeric(nsims) # Bootstrap method sampling variance for skewness
@@ -119,37 +117,49 @@ for(i in 1:nsims) {
 #Calculate and store the effect statistics for skewness and kurtosis
 
   if(type == "skewness") {
-       sk[i] = tryCatch((calc.skewness(x1) - calc.skewness(x2)), error = function(e) {return(NA)})
+       sk[i] = tryCatch((calc.skewness(x1) - calc.skewness(x2)), error = function(e) {return(NA)}) # small sample size corrected
     sk_sv[i] = tryCatch((calc.skewness(x1, output = "var") + calc.skewness(x2, output = "var")), error = function(e) {return(NA)})
 
+  # Do the computations and save to objects
+    x1_skew_delta <- tryCatch(skew_delta(x1), error = function(e) {return(NA)})
+    x2_skew_delta <- tryCatch(skew_delta(x2), error = function(e) {return(NA)})
+     x1_skew_boot <- tryCatch(boot_skew(x1),  error = function(e) {return(NA)})
+     x2_skew_boot <- tryCatch(boot_skew(x2),  error = function(e) {return(NA)})
+     x1_skew_jack <- tryCatch(jack_skew(x1),  error = function(e) {return(NA)})
+     x2_skew_jack <- tryCatch(jack_skew(x2),  error = function(e) {return(NA)})
+
   # Add in new methods for skewness
-        skew_delta[i] = tryCatch((skew_delta(x1)$est - skew_delta(x2)$est), error = function(e) {return(NA)})
-         boot_skew[i] = tryCatch((boot_skew(x1)$est - boot_skew(x2)$est), error = function(e) {return(NA)})
-         boot_skew_bc[i] = tryCatch((boot_skew(x1)$est_bc - boot_skew(x2)$est_bc), error = function(e) {return(NA)})
-         jack_skew[i] = tryCatch((jack_skew(x1)$est - jack_skew(x2)$est), error = function(e) {return(NA)})
-         jack_skew_bc[i] = tryCatch((jack_skew(x1)$est_bc - jack_skew(x2)$est_bc), error = function(e) {return(NA)})
+           skew_delta[i] = (x1_skew_delta$est - x2_skew_delta$est) # definition formula
+         boot_skew_bc[i] = (x1_skew_boot$est_bc - x2_skew_boot$est_bc)
+         jack_skew_bc[i] = (x1_skew_jack$est_bc - x2_skew_jack$est_bc)
 
   # Add in new methods for skewness sampling variance
-    skew_delta_sv[i] = tryCatch((skew_delta(x1)$var + skew_delta(x2)$var), error = function(e) {return(NA)})
-     boot_skew_sv[i] = tryCatch((boot_skew(x1)$var + boot_skew(x2)$var), error = function(e) {return(NA)})
-     jack_skew_sv[i] = tryCatch((jack_skew(x1)$var + jack_skew(x2)$var), error = function(e) {return(NA)})
+    skew_delta_sv[i] = (x1_skew_delta$var + x2_skew_delta$var)
+     boot_skew_sv[i] =  (x1_skew_boot$var + x2_skew_boot$var)
+     jack_skew_sv[i] =  (x1_skew_jack$var + x2_skew_jack$var)
   }
 
   if(type == "kurtosis") {
        ku[i] = tryCatch((calc.kurtosis(x1) - calc.kurtosis(x2)), error = function(e) {return(NA)})
     ku_sv[i] = tryCatch((calc.kurtosis(x1, output = "var") + calc.kurtosis(x2, output = "var")), error = function(e) {return(NA)}) 
+  
+  # Do the computations and save to objects
+    x1_kurt_delta <- tryCatch(kurt_delta(x1), error = function(e) {return(NA)})
+    x2_kurt_delta <- tryCatch(kurt_delta(x2), error = function(e) {return(NA)})
+    x1_kurt_boot <- tryCatch(boot_kurt(x1), error = function(e) {return(NA)})
+    x2_kurt_boot <- tryCatch(boot_kurt(x2), error = function(e) {return(NA)})
+    x1_kurt_jack <- tryCatch(jack_kurt(x1), error = function(e) {return(NA)})
+    x2_kurt_jack <- tryCatch(jack_kurt(x2), error = function(e) {return(NA)})
 
   # Add in new methods for kurtosis
-        kurt_delta[i] = tryCatch((kurt_delta(x1)$est - kurt_delta(x2)$est), error = function(e) {return(NA)})
-         boot_kurt[i] = tryCatch((boot_kurt(x1)$est - boot_kurt(x2)$est), error = function(e) {return(NA)})
-         boot_kurt_bc[i] = tryCatch((boot_kurt(x1)$est_bc - boot_kurt(x2)$est_bc), error = function(e) {return(NA)})
-         jack_kurt[i] = tryCatch((jack_kurt(x1)$est - jack_kurt(x2)$est), error = function(e) {return(NA)})
-         jack_kurt_bc[i] = tryCatch((jack_kurt(x1)$est_bc - jack_kurt(x2)$est_bc), error = function(e) {return(NA)})
-  
+           kurt_delta[i] =   (x1_kurt_delta$est - x2_kurt_delta$est)
+         boot_kurt_bc[i] = (x1_kurt_boot$est_bc - x2_kurt_boot$est_bc)
+         jack_kurt_bc[i] = (x1_kurt_jack$est_bc - x2_kurt_jack$est_bc)
+
   # Add in new methods for kurtosis sampling variance
-    kurt_delta_sv[i] = tryCatch((kurt_delta(x1)$var + kurt_delta(x2)$var), error = function(e) {return(NA)})
-     boot_kurt_sv[i] = tryCatch((boot_kurt(x1)$var + boot_kurt(x2)$var), error = function(e) {return(NA)})
-     jack_kurt_sv[i] = tryCatch((jack_kurt(x1)$var + jack_kurt(x2)$var), error = function(e) {return(NA)})
+    kurt_delta_sv[i] = (x1_kurt_delta$var + x2_kurt_delta$var)
+     boot_kurt_sv[i] =  (x1_kurt_boot$var + x2_kurt_boot$var)
+     jack_kurt_sv[i] =  (x1_kurt_jack$var + x2_kurt_jack$var)
   }
 }
 
@@ -173,14 +183,14 @@ if(type == "skewness") {
  mcse_bias_sv_delta_sk = sqrt(var(skew_delta_sv) / length(skew_delta_sv)), # Monte Carlo Standard error for bias of skewness 
 
     # Bootstrap method for skewness
-         bias_sk_boot = mean(boot_skew) - (params$skewness_g1 - params$skewness_g2), # Bias for skewness from bootstrap method
-    mcse_bias_boot_sk = sqrt(var(boot_skew) / length(boot_skew)), # Monte Carlo Standard error for bias of skewness from bootstrap method
+         bias_sk_boot_bc = mean(boot_skew_bc) - (params$skewness_g1 - params$skewness_g2), # Bias for skewness from bootstrap method
+    mcse_bias_boot_sk = sqrt(var(boot_skew_bc) / length(boot_skew_bc)), # Monte Carlo Standard error for bias of skewness from bootstrap method
       bias_sk_boot_sv = ((mean(boot_skew_sv) - sd(sk)^2) / sd(sk)^2)*100, # Relative Bias for skewness sampling variance from bootstrap method
  mcse_bias_sv_boot_sk = sqrt(var(boot_skew_sv) / length(boot_skew_sv)), # Monte Carlo Standard error for bias of skewness sampling variance from bootstrap method
 
     # Jackknife method for skewness  
-         bias_sk_jack = mean(jack_skew) - (params$skewness_g1 - params$skewness_g2), # Bias for skewness from jackknife method
-    mcse_bias_jack_sk = sqrt(var(jack_skew) / length(jack_skew)), # Monte Carlo Standard error for bias of skewness from jackknife method
+         bias_sk_jack_bc = mean(jack_skew_bc) - (params$skewness_g1 - params$skewness_g2), # Bias for skewness from jackknife method
+    mcse_bias_jack_sk = sqrt(var(jack_skew_bc) / length(jack_skew_bc)), # Monte Carlo Standard error for bias of skewness from jackknife method
       bias_sk_jack_sv = ((mean(jack_skew_sv) - sd(sk)^2) / sd(sk)^2)*100, # Relative Bias for skewness sampling variance from jackknife method
  mcse_bias_sv_jack_sk = sqrt(var(jack_skew_sv) / length(jack_skew_sv)), # Monte Carlo Standard error for bias of skewness sampling variance from jackknife method
 					   n_sims = length(sk_sv))) # Number of simulations
@@ -201,14 +211,14 @@ if(type == "kurtosis") {
 mcse_bias_sv_delta_ku = sqrt(var(kurt_delta_sv) / length(kurt_delta_sv)), # Monte Carlo Standard error for bias of kurtosis sampling variance from delta method
 
     # Bootstrap method for kurtosis
-         bias_ku_boot = mean(boot_kurt) - (params$kurtosis_g1 - params$kurtosis_g2), # Bias for kurtosis from bootstrap method
-    mcse_bias_boot_ku = sqrt(var(boot_kurt) / length(boot_kurt)), # Monte Carlo Standard error for bias of kurtosis from bootstrap method
+         bias_ku_boot_bc = mean(boot_kurt_bc) - (params$kurtosis_g1 - params$kurtosis_g2), # Bias for kurtosis from bootstrap method
+    mcse_bias_boot_ku_bc = sqrt(var(boot_kurt_bc) / length(boot_kurt_bc)), # Monte Carlo Standard error for bias of kurtosis from bootstrap method
       bias_ku_boot_sv = ((mean(boot_kurt_sv) - sd(ku)^2) / sd(ku)^2)*100, # Relative Bias for kurtosis sampling variance from bootstrap method
  mcse_bias_sv_boot_ku = sqrt(var(boot_kurt_sv) / length(boot_kurt_sv)), # Monte Carlo Standard error for bias of kurtosis sampling variance from bootstrap method
          
     # Jackknife method for kurtosis
-         bias_ku_jack = mean(jack_kurt) - (params$kurtosis_g1 - params$kurtosis_g2), # Bias for kurtosis from jackknife method
-    mcse_bias_jack_ku = sqrt(var(jack_kurt) / length(jack_kurt)), # Monte Carlo Standard error for bias of kurtosis from jackknife method   
+         bias_ku_jack_bc = mean(jack_kurt_bc) - (params$kurtosis_g1 - params$kurtosis_g2), # Bias for kurtosis from jackknife method
+    mcse_bias_jack_ku_bc = sqrt(var(jack_kurt_bc) / length(jack_kurt_bc)), # Monte Carlo Standard error for bias of kurtosis from jackknife method
       bias_ku_jack_sv = ((mean(jack_kurt_sv) - sd(ku)^2) / sd(ku)^2)*100, # Relative Bias for kurtosis sampling variance from jackknife method
  mcse_bias_sv_jack_ku = sqrt(var(jack_kurt_sv) / length(jack_kurt_sv)), # Monte Carlo Standard error for bias of kurtosis sampling variance from jackknife method
                n_sims = length(ku_sv)))
@@ -225,7 +235,7 @@ mcse_bias_sv_delta_ku = sqrt(var(kurt_delta_sv) / length(kurt_delta_sv)), # Mont
 # # Skewness simulation 
 ###------------------------------------------------------------------------###
 # Parameters
-                      nsims = 5000  # simulations, stick with 5000 min but maybe increase to 10,000
+                      nsims = 2500  # simulations, stick with 5000 min but maybe increase to 10,000
 	                        n = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 500)  
 	    							mean_g1 = c(0, 0)
 									  mean_g2 = c(0, 5)
@@ -257,7 +267,7 @@ n_per_group <- 10
 df_chunks <- split(scenarios, cut(seq_len(nrow(scenarios)), breaks = n_chunks, labels = FALSE))
 
 # Plot each chunk of scenarios. This will create a grid of plots for each scenario
-lapply(df_chunks, function(x) multi_plot_scenarios(x, folder = "output/figs/skewness/")) 
+#lapply(df_chunks, function(x) multi_plot_scenarios(x, folder = "output/figs/skewness/")) 
 
 # Create combinations of parameters expanded by sample size vector. Each row is a scenario with a sample size which is used to set up the simulation
 	params_all <- data.frame(tidyr::crossing(scenarios, n = n))               
@@ -274,7 +284,7 @@ for(i in 1:nrow(params_all)) {
   print(paste("Simulation for scenario", i, "completed. Bias_sk:", round(result_skewness$bias_sk[i], 2), "mcse_bias_sv_sk:", round(result_skewness$mcse_bias_sv_sk[i], 2), "mcse_bias_sk:", round(result_skewness$mcse_bias_sk[i], 2)))
 }
 )
-# 1096.327 seconds elapsed or 18 minutes
+# 1096.327 seconds elapsed or 18 minutes; sims = 10 for testing purposes takes about 13 min, so 5000 will take 8 hours; 4 for 2500 so will drop a bit
 # Merge the results with the scenario parameters
 result_skewness <- cbind(params_all, result_skewness)
 saveRDS(result_skewness, file = "./output/result_skewness.rds") # Save the results to a file
@@ -283,7 +293,7 @@ saveRDS(result_skewness, file = "./output/result_skewness.rds") # Save the resul
 # # Kurtosis simulation
 ###------------------------------------------------------------------------###
 	# Parameters
-                      nsims = 5000  # simulations, stick with 5000 min but maybe increase to 10,000
+                      nsims = 2500  # simulations, stick with 5000 min but maybe increase to 10,000
 								          n = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 500)  
 								    mean_g1 = c(0, 0)
 								    mean_g2 = c(0, 5)
