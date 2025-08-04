392dc982ee54c9d616949f2d6ee3ac0bffa1caf1
Daniel Noble <daniel.noble@anu.edu.au>
2025-08-04 12:24:14 +1000

    Adjust function to control axes

--- R/analysis.R
+++ R/analysis.R
@@ -16,8 +16,9 @@ rm(list = ls()) # Remove all objects from the environment
 	result_skew <- readRDS("./output/result_skewness.rds") # Load the skewness results
 
 # Plot function for the results
-plot_bias_violin <- function(data, y_var, y_lab) {
+plot_bias_violin <- function(data, y_var, y_lab, ylim = c(-1, 1)) {
   ggplot(data, aes(x = factor(n), y = .data[[y_var]], fill = factor(n))) + 
+    ylim(ylim) +
     geom_violin() + 
     geom_hline(aes(yintercept = 0), linetype = "dashed", color = "black") +
     labs(x = "Sample Size", y = TeX(y_lab)) +
@@ -36,21 +37,21 @@ plot_bias_violin <- function(data, y_var, y_lab) {
 ##------------------------------------------------------------------------##
 
 # Skewness plots
-bias_sk_plot        <- plot_bias_violin(result_skew, "bias_sk",         "Bias $\\Delta sk$")
-bias_sk_plot_delta  <- plot_bias_violin(result_skew, "bias_sk_delta",   "Bias $\\Delta sk$ (Delta Method)")
-bias_sk_plot_boot   <- plot_bias_violin(result_skew, "bias_sk_boot_bc", "Bias $\\Delta sk$ (Bootstrap)")
-bias_sk_plot_jack   <- plot_bias_violin(result_skew, "bias_sk_jack_bc", "Bias $\\Delta sk$ (Jackknife)")
+bias_sk_plot        <- plot_bias_violin(result_skew, "bias_sk",         "Bias $\\Delta sk$", ylim = c(-0.5, 0.5))
+bias_sk_plot_delta  <- plot_bias_violin(result_skew, "bias_sk_delta",   "Bias $\\Delta sk$ (Delta Method)", ylim = c(-0.5, 0.5))
+bias_sk_plot_boot   <- plot_bias_violin(result_skew, "bias_sk_boot_bc", "Bias $\\Delta sk$ (Bootstrap)", ylim = c(-0.5, 0.5))
+bias_sk_plot_jack   <- plot_bias_violin(result_skew, "bias_sk_jack_bc", "Bias $\\Delta sk$ (Jackknife)", ylim = c(-0.5, 0.5))
 
 # Kurtosis plots
-bias_ku_plot        <- plot_bias_violin(result_kurt, "bias_ku",         "Bias $\\Delta ku$")
-bias_ku_plot_delta  <- plot_bias_violin(result_kurt, "bias_ku_delta",   "Bias $\\Delta ku$ (Delta Method)")
-bias_ku_plot_boot   <- plot_bias_violin(result_kurt, "bias_ku_boot_bc", "Bias $\\Delta ku$ (Bootstrap)")
-bias_ku_plot_jack   <- plot_bias_violin(result_kurt, "bias_ku_jack_bc", "Bias $\\Delta ku$ (Jackknife)")
+bias_ku_plot        <- plot_bias_violin(result_kurt, "bias_ku",         "Bias $\\Delta ku$", ylim = c(-0.5, 0.5))
+bias_ku_plot_delta  <- plot_bias_violin(result_kurt, "bias_ku_delta",   "Bias $\\Delta ku$ (Delta Method)", ylim = c(-0.5, 0.5))
+bias_ku_plot_boot   <- plot_bias_violin(result_kurt, "bias_ku_boot_bc", "Bias $\\Delta ku$ (Bootstrap)", ylim = c(-0.5, 0.5))
+bias_ku_plot_jack   <- plot_bias_violin(result_kurt, "bias_ku_jack_bc", "Bias $\\Delta ku$ (Jackknife)", ylim = c(-0.5, 0.5))
 
 # Correlation plots
-bias_z_plot         <- plot_bias_violin(result_cor, "bias_d_cor",        "Bias $\\Delta Z_{r}$")
-bias_z_plot_boot    <- plot_bias_violin(result_cor, "bias_boot_d_cor",   "Bias $\\Delta Z_{r}$ (Bootstrapped)")
-bias_z_plot_jack    <- plot_bias_violin(result_cor, "bias_jack_d_cor",   "Bias $\\Delta Z_{r}$ (Jackknife)")
+bias_z_plot         <- plot_bias_violin(result_cor, "bias_d_cor",        "Bias $\\Delta Z_{r}$", ylim = c(-0.5, 0.5))
+bias_z_plot_boot    <- plot_bias_violin(result_cor, "bias_boot_d_cor",   "Bias $\\Delta Z_{r}$ (Bootstrapped)", ylim = c(-0.5, 0.5))
+bias_z_plot_jack    <- plot_bias_violin(result_cor, "bias_jack_d_cor",   "Bias $\\Delta Z_{r}$ (Jackknife)", ylim = c(-0.5, 0.5))
 
 # Combine all bias plots
 est_plot <- (bias_sk_plot| bias_sk_plot_delta | bias_sk_plot_boot | bias_sk_plot_jack) / (bias_ku_plot | bias_ku_plot_delta | bias_ku_plot_boot | bias_ku_plot_jack)  +
@@ -62,21 +63,21 @@ theme(plot.tag = element_text(size = 16, face = "bold"))
 ##------------------------------------------------------------------------##
 
 # Skewness
-bias_sv_sk       <- plot_bias_violin(result_skew, "bias_sk_sv",         "Relative Bias $SV_{\\Delta sk}$ (%)")
-bias_sv_sk_delta <- plot_bias_violin(result_skew, "bias_sk_delta_sv",   "Relative Bias $SV_{\\Delta sk}$ (%) (Delta method)")
-bias_sv_sk_boot  <- plot_bias_violin(result_skew, "bias_sk_boot_sv",    "Relative Bias $SV_{\\Delta sk}$ (%) (Bootstrap)")
-bias_sv_sk_jack  <- plot_bias_violin(result_skew, "bias_sk_jack_sv",    "Relative Bias $SV_{\\Delta sk}$ (%) (Jackknife)")
+bias_sv_sk       <- plot_bias_violin(result_skew, "bias_sk_sv",         "Relative Bias $SV_{\\Delta sk}$ (%)", ylim = c(-100, 100))
+bias_sv_sk_delta <- plot_bias_violin(result_skew, "bias_sk_delta_sv",   "Relative Bias $SV_{\\Delta sk}$ (%) (Delta method)", ylim = c(-100, 100))
+bias_sv_sk_boot  <- plot_bias_violin(result_skew, "bias_sk_boot_sv",    "Relative Bias $SV_{\\Delta sk}$ (%) (Bootstrap)", ylim = c(-100, 100))
+bias_sv_sk_jack  <- plot_bias_violin(result_skew, "bias_sk_jack_sv",    "Relative Bias $SV_{\\Delta sk}$ (%) (Jackknife)", ylim = c(-100, 100))
 
 # Kurtosis
-bias_sv_ku       <- plot_bias_violin(result_kurt, "bias_ku_sv",         "Relative Bias $SV_{\\Delta ku}$ (%)")
-bias_sv_ku_delta <- plot_bias_violin(result_kurt, "bias_ku_delta_sv",   "Relative Bias $SV_{\\Delta ku}$ (%) (Delta method)")
-bias_sv_ku_boot  <- plot_bias_violin(result_kurt, "bias_ku_boot_sv",    "Relative Bias $SV_{\\Delta ku}$ (%) (Bootstrap)")
-bias_sv_ku_jack  <- plot_bias_violin(result_kurt, "bias_ku_jack_sv",    "Relative Bias $SV_{\\Delta ku}$ (%) (Jackknife)")
+bias_sv_ku       <- plot_bias_violin(result_kurt, "bias_ku_sv",         "Relative Bias $SV_{\\Delta ku}$ (%)", ylim = c(-100, 100))
+bias_sv_ku_delta <- plot_bias_violin(result_kurt, "bias_ku_delta_sv",   "Relative Bias $SV_{\\Delta ku}$ (%) (Delta method)", ylim = c(-100, 100))
+bias_sv_ku_boot  <- plot_bias_violin(result_kurt, "bias_ku_boot_sv",    "Relative Bias $SV_{\\Delta ku}$ (%) (Bootstrap)", ylim = c(-100, 100))
+bias_sv_ku_jack  <- plot_bias_violin(result_kurt, "bias_ku_jack_sv",    "Relative Bias $SV_{\\Delta ku}$ (%) (Jackknife)", ylim = c(-100, 100))
 
 # Correlation
-bias_sv_z        <- plot_bias_violin(result_cor,  "bias_d_cor_sv",      "Relative Bias $SV_{\\Delta Z_{r}}$ (%)")
-bias_sv_z_boot   <- plot_bias_violin(result_cor,  "bias_boot_d_cor_sv", "Relative Bias $SV_{\\Delta Z_{r}}$ (%) (Bootstrapped)")
-bias_sv_z_jack   <- plot_bias_violin(result_cor,  "bias_jack_d_cor_sv", "Relative Bias $SV_{\\Delta Z_{r}}$ (%) (Jackknife)")
+bias_sv_z        <- plot_bias_violin(result_cor,  "bias_d_cor_sv",      "Relative Bias $SV_{\\Delta Z_{r}}$ (%)", ylim = c(-100, 100))
+bias_sv_z_boot   <- plot_bias_violin(result_cor,  "bias_boot_d_cor_sv", "Relative Bias $SV_{\\Delta Z_{r}}$ (%) (Bootstrapped)", ylim = c(-100, 100))
+bias_sv_z_jack   <- plot_bias_violin(result_cor,  "bias_jack_d_cor_sv", "Relative Bias $SV_{\\Delta Z_{r}}$ (%) (Jackknife)", ylim = c(-100, 100))
 
 # Combine all plots
 final_rel_bias_plot <- (bias_sv_sk | bias_sv_sk_delta | bias_sv_sk_boot | bias_sv_sk_jack) / (bias_sv_ku | bias_sv_ku_delta | bias_sv_ku_boot | bias_sv_ku_jack) +
